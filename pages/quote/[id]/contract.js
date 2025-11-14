6b6626c14b2faf7fba396e153fbecaad9ab43e19
newworldstrategiesai <newworldstrategiesai@gmail.com>
2025-11-13 20:07:15 -0600

    Hide 'Contract Signed' green section in PDF download - add no-print class

--- pages/quote/[id]/contract.js
+++ pages/quote/[id]/contract.js
@@ -195,7 +195,7 @@ export default function ContractPage() {
                 <p className="text-gray-600 dark:text-gray-400">Contract Number: {contractNumber}</p>
               </div>
               {isSigned && (
-                <div className="bg-green-50 dark:bg-green-900/20 border border-green-200 dark:border-green-800 rounded-lg p-4 text-center">
+                <div className="no-print bg-green-50 dark:bg-green-900/20 border border-green-200 dark:border-green-800 rounded-lg p-4 text-center">
                   <CheckCircle className="w-8 h-8 text-green-600 dark:text-green-400 mx-auto mb-2" />
                   <p className="text-green-800 dark:text-green-200 font-semibold">Contract Signed</p>
                   {contractData?.signed_at && (
